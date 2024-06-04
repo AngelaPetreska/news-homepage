21430e873089d2c4e1c9eeadd4fcede3a52d7901
Angela Petreska <angelapetreska997@gmail.com>
2024-06-04 17:59:17 +0200

    script.js was edited

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var menuOpenIcon = document.getElementById('menu-open-icon');
-    var menuCloseIcon = document.getElementById('menu-close-icon');
-    var toggleButton = document.querySelector('.menu-toggle');
-    var navigation = document.querySelector('.navigation');
+    let menuOpenIcon = document.getElementById('menu-open-icon');
+    let menuCloseIcon = document.getElementById('menu-close-icon');
+    let toggleButton = document.querySelector('.menu-toggle');
+    let navigation = document.querySelector('.navigation');
 
     toggleButton.addEventListener('click', function() {
-        var isOpen = navigation.classList.contains('open');
+        let isOpen = navigation.classList.contains('open');
         navigation.classList.toggle('open', !isOpen);
         menuOpenIcon.style.display = isOpen ? 'block' : 'none';
         menuCloseIcon.style.display = isOpen ? 'none' : 'block';
